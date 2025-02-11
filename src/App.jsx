6a39cd50027be0caf14ec487b306a2c78fe40353
sanish <s.karki1994@gmail.com>
2025-02-11 14:15:22 +0545

    Project setup, bootstrap installed and components run for demo purpose

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { Badge, Container, Navbar, Spinner } from 'react-bootstrap';
+
 function App() {
-  return <>Bootstrap Testing</>;
+  return (
+    <>
+      <h1>
+        Example heading <Badge bg='secondary'>New</Badge>
+      </h1>
+      <br />
+      <Spinner animation='grow' variant='success' />
+      <br />
+      <Navbar className='bg-body-tertiary'>
+        <Container>
+          <Navbar.Brand href='#home'>
+            <img
+              src='/assets/vite.svg'
+              width='30'
+              height='30'
+              className='d-inline-block align-top'
+              alt='React Bootstrap logo'
+            />
+            <span style={{ color: '#00a19c' }}> Bootstrap Testing</span>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default App;
